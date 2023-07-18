0a11f434041e2bb67e2a754f65df1b7ed3cf7dfd
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-18 18:11:27 -0500

    Add gitignore file

--- js/keys.js
+++
@@ -1,2 +0,0 @@
-const MAPBOX_TOKEN = "pk.eyJ1Ijoic3BmbHVnY29kZXVwIiwiYSI6ImNsZ3dnd2djNjFrZ2wzbnFpdzdjMDRhZG4ifQ.JcZeoMJPkDgCpLTXWdfrSA";
-const OPEN_WEATHER_APPID = "b5d882cfdc3850724933b5c961bd8de3";
