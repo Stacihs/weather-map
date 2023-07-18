a050dd4579ab84150adbfcca53f8aaa697befcfe
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-18 17:47:51 -0500

    Update weather_map.html and weather_map.js

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,37 +2,44 @@ $(() => {
     "use strict";
 
     /*****GLOBAL VARIABLES*****/
-    // const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+        // const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
     const OPEN_WEATHER_URL_FIVE_DAY = "https://api.openweathermap.org/data/2.5/forecast";
     const map = initializeMap();
     let userInputBox = document.querySelector('input');
+    let userInputBoxSearch = document.getElementById('place').innerText;
+
+
+    // TODO create loop to combine current and 5day forecast cards
+    // TODO create marker on map for current city
+    // TODO user can move marker or click on map and add marker
+    // TODO weather updates as marker is moved
 
 
     /*****FUNCTIONS*****/
-    // $.ajax(OPEN_WEATHER_URL, {
-    //     data: {
-    //         APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
-    //     }
-    // }).done((data) => {
-    //     console.log('current weather', data);
-    //     renderCurrentWeather(data);
-    // }).fail(console.error);
-    //
-    // // Renders current weather conditions
-    // const renderCurrentWeather = ((weatherStats) => {
-    //     $('#weatherInfo').append(`<div class="weatherCard">
-    //             <h1>${weatherStats.name}</h1>
-    //             <div><span id="temp">${parseInt(weatherStats.main.temp)}&deg;</span></div>
-    //             <h2>Feels Like</h2>
-    //             <div><span>${parseInt(weatherStats.main.feels_like)}&deg;</span></div>
-    //             <h2>Humidity</h2>
-    //             <div><span>${parseInt(weatherStats.main.humidity)}%</span></div>
-    //             <h2>Wind</h2>
-    //             <div><span>${weatherStats.wind.speed} mph</span></div>
-    //             <h2>Conditions</h2>
-    //             <div><span>${weatherStats.weather[0].description}</span></div>
-    //         </div>`);
-    // });
+    $.ajax(OPEN_WEATHER_URL, {
+        data: {
+            APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
+        }
+    }).done((data) => {
+        console.log('current weather', data);
+        renderCurrentWeather(data);
+    }).fail(console.error);
+
+    // Dynamically render html to DOM current forecast
+    const renderCurrentWeather = ((weatherStats) => {
+        $('#weatherInfoCurrent').append(`<div class="weatherCard">
+                <h1>${weatherStats.name}</h1>
+                <div><span id="temp">${parseInt(weatherStats.main.temp)}&deg;</span></div>
+                <h2>Feels Like</h2>
+                <div><span>${parseInt(weatherStats.main.feels_like)}&deg;</span></div>
+                <h2>Humidity</h2>
+                <div><span>${parseInt(weatherStats.main.humidity)}%</span></div>
+                <h2>Wind</h2>
+                <div><span>${weatherStats.wind.speed} mph</span></div>
+                <h2>Conditions</h2>
+                <div><span>${weatherStats.weather[0].description}</span></div>
+            </div>`);
+    });
 
 
     $.ajax(OPEN_WEATHER_URL_FIVE_DAY, {
@@ -86,16 +93,17 @@ $(() => {
             center: [-98.491142, 29.424349],
             zoom: 10
         }
-        geocode(userInput, MAPBOX_TOKEN).then((data) => {
+        geocode(userInputBox, MAPBOX_TOKEN).then((data) => {
             console.log(data);
             map.setCenter(data);
-            map.setZoom(15);
-            // const southerleighPopup = new mapboxgl.Popup()
-            //     .setHTML(`<p>Southerleigh Fine Food And Brewery</p>`)
-            const userMarker = new mapboxgl.Marker()
+            const userInputBoxPopup = new mapboxgl.Popup()
+                .setHTML(`<p>${userInputBoxSearch}</p>`)
+            const userInputBoxMarker = new mapboxgl.Marker()
                 .setLngLat(data)
                 .addTo(map)
-                // .setPopup(southerleighPopup);
+                .setPopup(userInputBoxPopup);
+
+
         })
 
         return new mapboxgl.Map(mapOptions);
@@ -103,10 +111,11 @@ $(() => {
 
 
     /*****EVENTS*****/
-     //When user types in search
-     userInputBox.addEventListener('keydown', userInputSearch());
+    //When user types in search
+    userInputBox.addEventListener('submit', userInputSearch());
 
-     /*****RUNS WHEN APP LOADS*****/
-     // userInputSearch();
+    /*****RUNS WHEN APP LOADS*****/
 });
+
+
 
