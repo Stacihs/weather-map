5ea15ae2b02cb0f4ee88e40044702972c0b04cac
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-18 20:42:02 -0500

    Add mapOptions variable to ajax call for current weather conditions

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,11 +2,10 @@ $(() => {
     "use strict";
 
     /*****GLOBAL VARIABLES*****/
-        // const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
     const OPEN_WEATHER_URL_FIVE_DAY = "https://api.openweathermap.org/data/2.5/forecast";
-    const map = initializeMap();
+    // const map = initializeMap();
     let userInputBox = document.querySelector('input');
-    let userInputBoxSearch = document.getElementById('place').innerText;
 
 
     // TODO create loop to combine current and 5day forecast cards
@@ -16,30 +15,49 @@ $(() => {
 
 
     /*****FUNCTIONS*****/
-    // $.ajax(OPEN_WEATHER_URL, {
-    //     data: {
-    //         APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
-    //     }
-    // }).done((data) => {
-    //     console.log('current weather', data);
-    //     renderCurrentWeather(data);
-    // }).fail(console.error);
-    //
-    // // Dynamically render html to DOM current forecast
-    // const renderCurrentWeather = ((weatherStats) => {
-    //     $('#weatherInfoCurrent').append(`<div class="weatherCard">
-    //             <h1>${weatherStats.name}</h1>
-    //             <div><span id="temp">${parseInt(weatherStats.main.temp)}&deg;</span></div>
-    //             <h2>Feels Like</h2>
-    //             <div><span>${parseInt(weatherStats.main.feels_like)}&deg;</span></div>
-    //             <h2>Humidity</h2>
-    //             <div><span>${parseInt(weatherStats.main.humidity)}%</span></div>
-    //             <h2>Wind</h2>
-    //             <div><span>${weatherStats.wind.speed} mph</span></div>
-    //             <h2>Conditions</h2>
-    //             <div><span>${weatherStats.weather[0].description}</span></div>
-    //         </div>`);
-    // });
+
+    // AJAX call for current weather in San Antonio creating map object with returned data
+    $.ajax(OPEN_WEATHER_URL, {
+        data: {
+            APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
+        }
+    }).done((data) => {
+        console.log('current weather', data);
+        renderCurrentWeather(data);
+        mapboxgl.accessToken = MAPBOX_TOKEN;
+
+        const mapOptions = {
+            container: 'map',
+            style: 'mapbox://styles/mapbox/navigation-day-v1',
+            center: [-98.491142, 29.424349],
+            zoom: 10
+        }
+        return new mapboxgl.Map(mapOptions);
+
+
+    }).fail(console.error);
+
+
+
+
+
+
+
+    // Dynamically render html to DOM current forecast
+    const renderCurrentWeather = ((weatherStats) => {
+        $('#weatherInfoCurrent').append(`<div class="weatherCard">
+                <h1>${weatherStats.name}</h1>
+                <div><span id="temp">${parseInt(weatherStats.main.temp)}&deg;</span></div>
+                <h2>Feels Like</h2>
+                <div><span>${parseInt(weatherStats.main.feels_like)}&deg;</span></div>
+                <h2>Humidity</h2>
+                <div><span>${parseInt(weatherStats.main.humidity)}%</span></div>
+                <h2>Wind</h2>
+                <div><span>${weatherStats.wind.speed} mph</span></div>
+                <h2>Conditions</h2>
+                <div><span>${weatherStats.weather[0].description}</span></div>
+            </div>`);
+    });
 
 
     $.ajax(OPEN_WEATHER_URL_FIVE_DAY, {
@@ -71,51 +89,31 @@ $(() => {
         });
     });
 
-    function initializeMap() {
-        mapboxgl.accessToken = MAPBOX_TOKEN;
-
-        const mapOptions = {
-            container: 'map',
-            style: 'mapbox://styles/mapbox/navigation-day-v1',
-            center: [-98.491142, 29.424349],
-            zoom: 10
-        }
-        return new mapboxgl.Map(mapOptions);
-    }
-
 
     // Map
-    function userInputSearch() {
-
-        const mapOptions = {
-            container: 'map',
-            style: 'mapbox://styles/mapbox/navigation-day-v1',
-            center: [-98.491142, 29.424349],
-            zoom: 10
-        }
-        geocode(userInputBox, MAPBOX_TOKEN).then((data) => {
-            console.log(data);
-            map.setCenter(data);
-            const userInputBoxPopup = new mapboxgl.Popup()
-                .setHTML(`<p>${userInputBoxSearch}</p>`)
-            const userInputBoxMarker = new mapboxgl.Marker()
-                .setLngLat(data)
-                .addTo(map)
-                .setPopup(userInputBoxPopup);
-
-
-        })
-
-        return new mapboxgl.Map(mapOptions);
-    }
+    // function userInputSearch() {
+    //
+    //     geocode(userInputBox, MAPBOX_TOKEN).then((data) => {
+    //         console.log(data);
+    //          map.setCenter(data);
+    //         const userInputBoxPopup = new mapboxgl.Popup()
+    //             .setHTML(`<p>${userInputBoxSearch}</p>`)
+    //         const userInputBoxMarker = new mapboxgl.Marker()
+    //             .setLngLat(data)
+    //             .addTo(map)
+    //             .setPopup(userInputBoxPopup);
+    //     })
+    //
+    //     return new mapboxgl.Map(mapOptions);
+    // }
 
 
     /*****EVENTS*****/
     //When user types in search
-    userInputBox.addEventListener('submit', userInputSearch());
+    userInputBox.addEventListener('HTMLInputElement', userInputSearch());
 
     /*****RUNS WHEN APP LOADS*****/
-});
+})();
 
 
 
