2551d5a485fc08b4f583784c5aae4e83346cec17
Staci Holmes-Segarra <staci.holmessegarra@gmail.com>
2023-07-19 05:24:43 -0500

    Tidy up code

--- js/weather_map.js
+++ js/weather_map.js
@@ -7,10 +7,6 @@ $(() => {
     const map = initializeMap();
     let userInput = document.getElementById('place').value;
 
-
-    // TODO create marker on map for current city
-    // TODO user can move marker or click on map and add marker
-    // TODO weather updates as marker is moved
     /*****FUNCTIONS*****/
     function initializeMap() {
         mapboxgl.accessToken = MAPBOX_TOKEN;
@@ -26,37 +22,41 @@ $(() => {
 
 
     // AJAX call for current weather in San Antonio creating map object with returned data
-    $.ajax(OPEN_WEATHER_URL, {
-        data: {
-            APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
-        }
-    }).done((data) => {
-        console.log('current weather', data);
-        renderCurrentWeather(data);
 
-        mapboxgl.accessToken = MAPBOX_TOKEN;
-        const mapOptions = {
-            container: 'map',
-            style: 'mapbox://styles/mapbox/navigation-day-v1',
-            center: [-98.491142, 29.424349],
-            zoom: 10
-        }
-        const marker = new mapboxgl.Marker({
-            draggable: true
-        }).setLngLat([-98.491142, 29.424349])
-            .addTo(map);
+        $.ajax(OPEN_WEATHER_URL, {
+            data: {
+                APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
+            }
+        }).done((data) => {
+            console.log('current weather', data);
+            renderCurrentWeather(data);
 
-    }).fail(console.error);
+            mapboxgl.accessToken = MAPBOX_TOKEN;
+            const mapOptions = {
+                container: 'map',
+                style: 'mapbox://styles/mapbox/navigation-day-v1',
+                center: [-98.491142, 29.424349],
+                zoom: 10
+            }
+
+            const marker = new mapboxgl.Marker({
+                draggable: true
+            }).setLngLat([-98.491142, 29.424349])
+                .addTo(map);
+        }).fail(console.error);
 
 
-    $.ajax(OPEN_WEATHER_URL_FIVE_DAY, {
-        data: {
-            APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
-        }
-    }).done((data) => {
-        console.log(data);
-        renderFiveDayForecast(data);
-    }).fail(console.error);
+
+
+        $.ajax(OPEN_WEATHER_URL_FIVE_DAY, {
+            data: {
+                APPID: OPEN_WEATHER_APPID, lat: 29.423017, lon: -98.48527, units: "imperial"
+            }
+        }).done((data) => {
+            console.log(data);
+            renderFiveDayForecast(data);
+        }).fail(console.error);
+
 
 
     // Dynamically render html to DOM current forecast
@@ -100,18 +100,19 @@ $(() => {
 
     // User search for different city weather
     function userSearch() {
-        geocode(userInput, MAPBOX_TOKEN).then((data) => {
+        geocode('Seattle', MAPBOX_TOKEN).then((data) => {
             console.log(data);
             map.setCenter(data);
         })
-    }
+    };
+
 
     /*****EVENTS*****/
-    //When user types in search
-    document.querySelector('button').addEventListener('click', userSearch);
+    //Event listener on submit button
+    document.querySelector('#submit').addEventListener('submit', userSearch);
 
     /*****RUNS WHEN APP LOADS*****/
-});
+})();
 
 
 
